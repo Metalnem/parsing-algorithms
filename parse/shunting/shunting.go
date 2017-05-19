c73bd87f263e3baf0f9140182f40e58f4f3e7802
Nemanja Mijailovic <metalnem@mijailovic.net>
2017-05-19 16:38:38 +0200

    Implement helper functions

--- parse/shunting/shunting.go
+++ parse/shunting/shunting.go
@@ -54,12 +54,12 @@ func New() parse.Parser {
 }
 
 func (parser) Parse(input string) (ast.Expr, error) {
-	s := scan.NewScanner(input)
+	state := &state{
+		s:   scan.NewScanner(input),
+		ops: []op{op{}},
+	}
 
-	state := &state{s: s}
-	expr, err := state.parseExpr()
-
-	if err != nil {
+	if err := state.parseExpr(); err != nil {
 		return nil, err
 	}
 
@@ -67,9 +67,67 @@ func (parser) Parse(input string) (ast.Expr, error) {
 		return nil, errors.Errorf("Expected EOF, got %s", next.Value)
 	}
 
-	return expr, nil
+	return state.exprs[0], nil
 }
 
-func (s *state) parseExpr() (ast.Expr, error) {
-	return nil, nil
+func (s *state) parseExpr() error {
+	return nil
+}
+
+func (s *state) parsePrimary() error {
+	return nil
+}
+
+func (s *state) push(op op) {
+	for greater(s.ops[len(s.ops)-1], op) {
+		s.pop()
+	}
+
+	s.ops = append(s.ops, op)
+}
+
+func (s *state) pop() {
+	op := s.ops[len(s.ops)-1]
+	s.ops = s.ops[:len(s.ops)-1]
+
+	if op.kind == binary {
+		y := s.exprs[len(s.exprs)-1]
+		s.exprs = s.exprs[:len(s.exprs)-1]
+
+		x := s.exprs[len(s.exprs)-1]
+		s.exprs[len(s.exprs)-1] = &ast.BinaryExpr{Op: op.value, X: x, Y: y}
+	} else {
+		x := s.exprs[len(s.exprs)-1]
+		s.exprs[len(s.exprs)-1] = &ast.UnaryExpr{Op: op.value, X: x}
+	}
+}
+
+func makeUnary(s string) (op, bool) {
+	return makeOp(s, unary)
+}
+
+func makeBinary(s string) (op, bool) {
+	return makeOp(s, binary)
+}
+
+func makeOp(s string, kind kind) (op, bool) {
+	for _, op := range ops {
+		if op.value == s && op.kind == kind {
+			return op, true
+		}
+	}
+
+	return op{}, false
+}
+
+func greater(x, y op) bool {
+	if x.kind == binary && y.kind == binary {
+		return x.prec > y.prec || (x.assoc == left && x.prec == y.prec)
+	}
+
+	if x.kind == unary && y.kind == binary {
+		return x.prec >= y.prec
+	}
+
+	return false
 }
